efb17fbbc830626b8198905a11c98a1a55f6bf42
mgieser90 <153868375+mgieser90@users.noreply.github.com>
2024-01-16 15:55:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const displayText = document.getElementById('display-text');
 const easterEggText = document.getElementById('display-eetext');
 const numberButtons = document.querySelectorAll('.button-number');
 const clearButton = document.getElementById('clear');
-
+const delButton = document.getElementById('backspace');
 
 numberButtons.forEach(numberButton => {
   numberButton.addEventListener('click', () => {
@@ -12,4 +12,8 @@ numberButtons.forEach(numberButton => {
 
 clearButton.addEventListener('click', () => {
   displayText.textContent = '';
+});
+
+delButton.addEventListener('click', () => {
+  displayText.textContent = displayText.textContent.slice(0, -1);
 });
